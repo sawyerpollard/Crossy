007d21e2b507dd66d5d85271a9d3e71ec6c02c6a
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-07-28 14:41:27 -0400

    Disable wrapping on clue banner

--- src/components/ClueBanner.tsx
+++ src/components/ClueBanner.tsx
@@ -9,7 +9,7 @@ export default function ClueBanner(props: ClueBannerProps) {
     const directionSymbol = props.clue.direction === Direction.Across ? 'A' : 'D';
     return (
         <div className="flex items-center px-4 py-2 bg-blue-200 min-h-[4rem]">
-            <p className="font-bold">{props.clue.number + directionSymbol}</p>
+            <p className="font-bold whitespace-nowrap">{props.clue.number + directionSymbol}</p>
             <p className="px-4">{props.clue.message}</p>
         </div>
     );
